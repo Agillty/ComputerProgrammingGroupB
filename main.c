c6b79df44dadf33ede6d93546c469ef031e050e9
AngusHsu <80775003h@ntnu.edu.tw>
2025-11-24 22:59:36 +0800

    floatAdd

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 int add(int x, int y);
+float fadd(float x, float y);
 int sub(int x, int y);
 int mult(int x, int y);
 
@@ -9,11 +10,18 @@ int main() {
     int a = 5;
     int b = 10;
     int sum = add(a, b);
+    float fa= 12.5;
+    float fb=5.2;
     printf("Sum of %d and %d is %d\n", a, b, sum);
+    printf("Sum of %f and %f is %f\n",fa, fb, fadd(fa,fb));
     printf("Ans of %d - %d is %d\n", a, b, sub(a,b));
     return 0;
 }
 int add(int x, int y) {
+    return x + y;
+}
+float fadd(float x, float y)
+{
     return x + y;
 }
 int sub(int x, int y) {
