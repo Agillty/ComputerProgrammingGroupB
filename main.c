9bd50f7331b97e6bc24fe0687fea28f508fd86e3
AngusHsu <80775003h@ntnu.edu.tw>
2025-11-25 10:09:20 +0800

    mult and div for int

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ float fadd(float x, float y);
 int sub(int x, int y);
 float fsub(float x, float y);
 int mult(int x, int y);
-
+int div(int x, int y);
 
 int main() {
     int a = 5;
@@ -18,6 +18,8 @@ int main() {
     printf("Sum of %f and %f is %f\n",fa, fb, fadd(fa,fb));
     printf("Ans of %d - %d is %d\n", a, b, sub(a,b));
     printf("Ans of %f - %f is %f\n", fa, fb, fsub(fa,fb));
+    printf("Product of %d and %d is %d\n", a, b, mult(a,b));
+    printf("Division of %d and %d is %d\n", b, a, div(b,a));
     return 0;
 }
 int add(int x, int y) {
@@ -32,4 +34,10 @@ int sub(int x, int y) {
 }
 float fsub(float x, float y) {
     return x - y;
+}
+int mult(int x, int y) {
+    return x * y;
+}
+int div(int x, int y) {
+    return x / y;
 }
